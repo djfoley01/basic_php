0635319c229e2a157647d3e91af3fe5ef9b0b0d9
Daniel Foley <daniel@Daniels-MacBook-Pro.local>
2018-03-23 12:26:24 -0500

    updated ocp control

--- ocp-control.sh
+++ ocp-control.sh
@@ -3,3 +3,10 @@
 # update_obj determines whether or not modifications should be applied to the application objects in OpenShift.
 # This will replace the route and deployment configuration settings, and delete then re-create the service if set to yes
 export update_obj=no
+
+if [ "$update_obj" = "no" ]; then
+	/usr/local/bin/oc rollout latest dc/basicphp -n examples
+else
+	echo "Replacing Objects"
+	/usr/local/bin/oc replace -f route.json
+fi
