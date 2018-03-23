d4c69555084c44b4a704c7ef773f02edf8b546ad
Daniel Foley <daniel@Daniels-MacBook-Pro.local>
2018-03-23 12:30:23 -0500

    test script

--- ocp-control.sh
+++ ocp-control.sh
@@ -2,7 +2,7 @@
 
 # update_obj determines whether or not modifications should be applied to the application objects in OpenShift.
 # This will replace the route and deployment configuration settings, and delete then re-create the service if set to yes
-export update_obj=no
+export update_obj=yes
 
 if [ "$update_obj" = "no" ]; then
 	/usr/local/bin/oc rollout latest dc/basicphp -n examples
